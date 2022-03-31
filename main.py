d8b69f095fc121c20a2b5c31107a2e18f85c9e0a
qwazar14 <zenez89@gmail.com>
2022-03-31 17:13:20 +0300

    Added subplots

--- main.py
+++ main.py
@@ -95,3 +95,24 @@ pyplot.imshow(image)
 # plot mask
 pyplot.imshow(mask[:, :, 0], cmap='gray', alpha=0.5)
 pyplot.show()
+
+# plot first few images
+for i in range(9):
+    # define subplot
+    pyplot.subplot(330 + 1 + i)
+    # plot raw pixel data
+    image = train_set.load_image(i)
+    pyplot.imshow(image)
+    # plot all masks
+    mask, _ = train_set.load_mask(i)
+    for j in range(mask.shape[2]):
+        pyplot.imshow(mask[:, :, j], cmap='gray', alpha=0.3)
+# show the figure
+pyplot.show()
+
+# enumerate all images in the dataset
+for image_id in train_set.image_ids:
+    # load image info
+    info = train_set.image_info[image_id]
+    # display on the console
+    print(info)
